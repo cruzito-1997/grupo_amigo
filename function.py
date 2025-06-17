48feccf34a239f9303957874222acdd89e17e79e
andres cruz <andrescamilocruzloaiza@gmail.com>
2025-06-16 20:35:41 -0400

    def/andres

---
+++ function.py
@@ -0,0 +1,18 @@
+# Trabajo Colaborativo: "Mini Agenda de Contactos"
+# Objetivo:
+# Desarrollar una pequeña aplicación de consola en Python que funcione como una agenda de contactos. El objetivo es aplicar los conocimientos de programación estructurada (variables, constantes, tipos de datos, estructuras de control, funciones, etc.) y prácticas básicas de trabajo colaborativo con Git y GitHub.
+# ________________________________________
+# Requisitos del Proyecto
+# La aplicación debe permitir al usuario:
+
+# Debe usarse un menú para navegar entre estas opciones.
+# La estructura de datos puede ser una lista de diccionarios.
+# ________________________________________
+# Organización del Trabajo
+# •	El repositorio debe ser creado por uno de los integrantes del grupo y compartido con los otros.
+# •	Todos deben clonar el repositorio en su computadora local.
+# •	Cada alumno trabajará en una rama propia (ejemplo: rama-juan, rama-maria, rama-carlos).
+# •	Cada uno será responsable de una parte del programa:
+# o	Alumno 1: Función para agregar y mostrar contactos.
+# o	Alumno 2: Función para buscar y eliminar contactos.
+# o	Alumno 3: Estructura del menú principal y control de flujo del programa.
